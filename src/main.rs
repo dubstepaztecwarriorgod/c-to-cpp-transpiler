000dacb5774aedbbddc6db76a3c80ede4542315c
dubstepaztecwarriorgod <dubstepaztec35@gmail.com>
2024-09-25 10:04:41 -0700

    booty fwart

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::{env, fs};
+use std::path::PathBuf;
+
+fn main() {
+    let path = env::args()
+        .nth(1)
+        .expect("A file path arg was never provided");
+    
+    let mut sigma_path = PathBuf::from(&path);
+
+    if sigma_path.set_extension("cpp") {
+        fs::rename(path, sigma_path).unwrap()
+    }
+}
